03a8f5b4a66a824b04f2fd636f4ba24213c55270
kscottcharles <156074188+kscottcharles@users.noreply.github.com>
2024-01-09 16:10:13 -0600

    change name

---
+++ Assignment 1.R
@@ -0,0 +1,59 @@
+#Assignment 1
+
+#Install tidyverse and papaja packages
+
+install.packages("tidyverse")
+library(tidyverse)
+
+install.packages("papaja")
+library(papaja)
+
+#1+ Packages of your choice
+install.packages("data.table")
+library(data.table)
+
+install.packages("DescTools")
+library(DescTools)
+
+install.packages("ggsci")
+library(ggsci)
+
+install.packages("here")
+library(here)
+
+install.packages("kableExtra")
+library(kableExtra)
+
+install.packages("lme4")
+library(lme4)
+
+install.packages("psych")
+library(psych)
+
+install.packages("RColorBrewer")
+library(RColorBrewer)
+
+install.packages("Require")
+library(Require)
+
+install.packages("scales")
+library(scales)
+
+install.packages("stats")
+library(stats)
+
+#Assigns 1+ numeric variable and 1+ string/character variable
+
+#assigning numeric value
+x = 10 
+x
+class(x)
+
+
+#assigning string/character variable
+Q1 <- c("Agree", "Strongly Agree", "Disagree", "Neither", "Agree", "Neither")
+Q1
+
+#BONUS: Create a dataframe
+fruits <- data.frame(fruits = c("apple", "pineapple", "orange", "pear"))
+
